b7be671c3fa3c63a228d91c057bda7d0efb8f933
Сергій <sergiip1994@gmail.com>
2023-08-15 22:29:50 +0300

    hw-1

---
+++ hw1/main.js
@@ -0,0 +1,45 @@
+let s1 ='hello';
+let s2 ='own';
+let s3 ='com';
+let s4 ='ua';
+let n1 = 1;
+let n2 = 10;
+let n3 = -999;
+let n4 = 123;
+let n5 = 3.14;
+let n6 = 2.7;
+let n7 = 16;
+let b1 = true;
+let b2 = false;
+
+console.log(s1);
+console.log(s2);
+console.log(s3);
+console.log(s4);
+console.log(n1);
+console.log(n2);
+console.log(n3);
+console.log(n4);
+console.log(n5);
+console.log(n6);
+console.log(n7);
+console.log(b1);
+console.log(b2);
+
+let firstName = 'Panchuk';
+let middleName = 'Sergii';
+let lastName = 'Pavlovich';
+
+console.log(firstName + ' ' + middleName + ' ' + lastName);
+console.log(`${firstName} ${middleName} ${lastName}`);
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a, typeof b, typeof c)
+
+let name = prompt('your name');
+let secondName = prompt('your last name');
+let age = prompt('your age')
+console.log(`${name} ${secondName} ${age}`)
